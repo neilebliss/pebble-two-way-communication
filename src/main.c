0fddbddc2f315df419fc72f57c2d558ee850f799
neilebliss <neil@neilbliss.com>
2013-10-11 16:00:35 -0700

    broken - in progress.

--- src/main.c
+++ src/main.c
@@ -5,13 +5,16 @@
 
 
 #define MY_UUID { 0x03, 0x7E, 0xE1, 0x06, 0x16, 0x01, 0x43, 0x39, 0x98, 0x52, 0x5E, 0xC4, 0x49, 0xB2, 0x68, 0xD3 }
-PBL_APP_INFO_SIMPLE(MY_UUID, "Button App", "Demo Corp", 1 /* App version */);
+PBL_APP_INFO_SIMPLE(MY_UUID, "2way comms", "Yoda dev", 1 /* App version */);
 
 
-Window window;
-
-TextLayer textLayer;
-
+static struct YodaData
+{
+	Window window;
+	TextLayer textLayer;
+	AppSync sync;
+	char *message;
+} yoda_data;
 
 // Modify these common button handlers
 
@@ -65,23 +68,32 @@ void click_config_provider(ClickConfig **config, Window *window) {
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
-
-  window_init(&window, "Button App");
+  Window* window = &yoda_data.window;
+  window_init(window, "2way comms");
   window_stack_push(&window, true /* Animated */);
 
-  text_layer_init(&textLayer, window.layer.frame);
-  text_layer_set_text(&textLayer, "Hello World");
-  text_layer_set_font(&textLayer, fonts_get_system_font(FONT_KEY_GOTHAM_30_BLACK));
-  layer_add_child(&window.layer, &textLayer.layer);
+	TextLayer* textLayer = &yoda_data.textLayer;
+  text_layer_init(textLayer, window.layer.frame);
+  text_layer_set_text(textLayer, "Press SELECT to update remote");
+  text_layer_set_font(textLayer, fonts_get_system_font(FONT_KEY_GOTHAM_30_BLACK));
+  layer_add_child(&window->layer, &textLayer->layer);
 
   // Attach our desired button functionality
-  window_set_click_config_provider(&window, (ClickConfigProvider) click_config_provider);
+  window_set_click_config_provider(window, (ClickConfigProvider) click_config_provider);
+		
+	Tuplet initial_values[] = {
+		TupletCString(0x0, "Initial Message")
+	}
+	
+	app_sync_init()
+
 }
 
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
+    .init_handler = &handle_init,
+	  .deinit_handler = &handle_deinit
   };
   app_event_loop(params, &handlers);
 }
